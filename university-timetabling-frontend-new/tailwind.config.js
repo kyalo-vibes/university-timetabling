3eecb34c236a516c2e6e65bd951b8cfdd24dc740
Rioba Ian <riobafelix@gmail.com>
2023-05-22 08:22:27 +0300

    add daisy ui for tailwind

--- university-timetabling-frontend-new/tailwind.config.js
+++ university-timetabling-frontend-new/tailwind.config.js
@@ -1,8 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light"],
   },
-  plugins: [],
 };
